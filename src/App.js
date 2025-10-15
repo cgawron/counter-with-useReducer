7290ede3a656c345f8075ab4bd6de2de3b89d015
Christian Gawron <gawron.christian@fh-swf.de>
2025-10-15 16:20:16 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useReducer } from "react";
+import Counter from "./Counter";
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + action.payload;
+    case "decrement":
+      return state - action.payload;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [counter, dispatch] = useReducer(reducer, initialState);
+  return <Counter counter={counter} dispatch={dispatch} />;
+}
+
+export default App;
